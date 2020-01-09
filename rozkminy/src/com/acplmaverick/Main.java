45f87270d942aed6582b939285f03b87414ac929
Marcin Wawrzonowski <MaverickCCCP@Gmail.com>
2020-01-09 22:47:29 +0100

    rozkminy

---
+++ rozkminy/src/com/acplmaverick/Main.java
@@ -0,0 +1,53 @@
+package com.acplmaverick;
+
+public class Main
+{
+                  //typ zwracany      //nazwa     //lista argumentow
+    public static void                funkcja     (int argument1, String argument2)
+    {   // blok kodu w klamerkach
+        System.out.println("Dupa.");
+        System.out.println(argument1);
+        System.out.println(argument2);
+    }
+
+    public static void funkcja(int argument1)
+    {
+        System.out.println("Lol.");
+        System.out.println(argument1);
+    }
+
+    public static int bar(int argument)
+    {
+        if(argument < 0)
+        {
+            return argument;
+        }
+
+        argument = argument * 2;
+        return argument;
+    }
+
+    public static void main(String[] args)
+    {
+        int a = 5;
+
+        boolean val = false;
+
+        switch (a)
+        {
+            case 1:
+                System.out.println("Jeden");
+                break;
+            case 2:
+                System.out.println("Dwa");
+                break;
+            default:
+                System.out.println("Nie wiem");
+                break;
+        }
+
+        System.out.println(bar(5));
+
+        //funkcja(a);
+    }
+}
