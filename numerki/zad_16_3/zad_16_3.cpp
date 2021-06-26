6385b5fe2ff652d235e8979378f95de7c747dbdb
Marcin Wawrzonowski <maverickcccp@gmail.com>
2021-06-26 14:55:53 +0200

    numerki zad_16_3 Simpson

--- numerki/zad_16_3/zad_16_3.cpp
+++ numerki/zad_16_3/zad_16_3.cpp
@@ -19,6 +19,8 @@ void integrate_rectangles(const double x_min, const double x_max, const double r
     const double eps)
 {
     // http://theflyingkeyboard.net/algorithms/midpoint-rule-rectangle-method-algorithm/
+    // http://theflyingkeyboard.net/c/c-midpoint-rule-rectangle-method-2/
+
     double result = 0.0;
     int num_iterations = 1;
     bool is_exact = false;
@@ -38,14 +40,50 @@ void integrate_rectangles(const double x_min, const double x_max, const double r
         result = (length / (double)num_iterations) * sum;
 
         is_exact = compare(result, result_analytic, eps, num_iterations, "Rectangles");
-        ++num_iterations;
+        ++num_iterations; // Might be faster to increase num_iterations non-linearly.
     } while (is_exact == false);
 }
 
 void integrate_simpson(const double x_min, const double x_max, const double result_analytic,
     const double eps)
 {
+    // https://eduinf.waw.pl/inf/alg/004_int/0004.php
 
+    double result = 0.0;
+    int num_iterations = 1;
+    bool is_exact = false;
+
+    do
+    {
+        const double length = x_max - x_min; // Length of section.
+        const double result_0 = function(x_min);
+        const double result_n = function(x_max);
+        
+        double sum_f = 0.0; // Sum of function values at the beginning of each section.
+        double sum_t = 0.0; // Sum of function values at the middle of each section.
+
+        for(int i = 1; i <= num_iterations; ++i)
+        {
+            const double x_i = x_min + ((double)i / (double)num_iterations) * length;
+            const double x_i_min_1 = x_min + ((double)(i - 1) / (double)num_iterations) * length;
+            const double t_i = (x_i_min_1 + x_i) / 2.0;
+
+            sum_t += function(t_i);
+
+            if(i == num_iterations)
+            {
+                continue;
+            }
+
+            sum_f += function(x_i);
+        }
+
+        // Combine sums with constant parameters.
+        result = (length / (6.0 * (double)num_iterations)) * (result_0 + result_n + 2.0 * sum_f + 4.0 * sum_t);
+        
+        is_exact = compare(result, result_analytic, eps, num_iterations, "Simpson");
+        ++num_iterations; // Might be faster to increase num_iterations non-linearly.
+    } while (is_exact == false);
 }
 
 int main(int argc, const char** argv)
@@ -60,6 +98,9 @@ int main(int argc, const char** argv)
     std::cout << "Analytic result : [" << result_analytic << "]\n";
 
     integrate_rectangles(x_min, x_max, result_analytic, eps);
+
+    std::cout << "\n";
+
     integrate_simpson(x_min, x_max, result_analytic, eps);
 
     return 0;
